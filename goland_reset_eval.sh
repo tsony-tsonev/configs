13b048a3753af057b40448cba62fd66207405202
tsony <tsonevtsony@gmail.com>
2019-11-27 11:18:07 +0200

    goland: update reset eval to work on newer versions

--- goland_reset_eval.sh
+++ goland_reset_eval.sh
@@ -2,5 +2,6 @@
 set -x
 
 rm ~/$1/config/eval/GoLand*.evaluation.key
-sed -i '/evlsprt/d' ~/$1/config/options/options.xml 
+#sed -i '/evlsprt/d' ~/$1/config/options/options.xml #for versions before 2019
+sed -i '/evlsprt/d' ~/$1/config/options/other.xml 
 rm -rf ~/.java/.userPrefs/jetbrains/goland
